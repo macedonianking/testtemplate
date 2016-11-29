d12d06be379d0997d0d769c1732d734a37ca2867
lhb <lhb@oppo.com>
2016-11-29 20:52:34 +0800

    update configs.

--- src/chapter12/chapter12.cpp
+++ src/chapter12/chapter12.cpp
@@ -1,85 +1,15 @@
 #include "chapter12/chapter12.h"
 
-SalesItem12::SalesItem12()
-: mCount(0),
-  mRevenue(0) {
-}
+namespace chapter12 {
 
-SalesItem12::SalesItem12(const std::string &isbn, int count, double revenue)
-: mISBN(isbn),
-  mCount(count),
-  mRevenue(revenue) 
-{
-}
-
-SalesItem12::SalesItem12(const char *isbn, int count, double revenue)
-: mISBN(isbn),
-  mCount(count),
-  mRevenue(revenue) 
-{
-}
-
-void SalesItem12::Dump(std::ostream &out) const {
-	out << "void SalesItem12::Dump(std::ostream &) const";
-}
-
-void SalesItem12::Dump(std::ostream &out) {
-	out << "void SalesItem12::Dump(std::ostream &)";
-}
-
-SalesItem12 &SalesItem12::operator=(const SalesItem12& rhs) {
-    if (&rhs != this) {
-        mISBN = rhs.mISBN;
-        mCount = rhs.mCount;
-        mRevenue = rhs.mRevenue;
-    } 
-    return *this;
-}
-
-Person::Person() {
-}
-
-Person::Person(const char *name, const char *address)
-: mName(name),
-  mAddress(address) {
-}
-
-void chapter12_1_tutorial() {
-	SalesItem12 item("KKKFR", 10, 200303.0);
-	((const SalesItem12&)item).Dump(std::cout);
-	std::cout << std::endl;
-}
-
-Screen::Screen(int width, int height)
-: mWidth(width),
-  mHeight(height),
-  mContents(width * height, ' ') {
-    this->AdjustCursor();
-}
-
-void Screen::AdjustCursor() {
-    if (mCursorX >= mWidth) {
-        mCursorX = mWidth - 1;
-    } else if(mCursorX < 0) {
-        mCursorX = 0;
-    }
-    if (mCursorY >= mHeight) {
-        mCursorY = mHeight - 1;
-    } else if(mCursorY < 0) {
-        mCursorY = 0;
+double SalesItem::Average() const {
+    if (!mCount) {
+        return 0.0;
+    } else {
+        return mRevenue / mCount;
     }
 }
 
-Screen::Screen(const Screen &rhs)
-: mWidth(rhs.mWidth),
-  mHeight(rhs.mHeight),
-  mCursorX(rhs.mCursorX),
-  mCursorY(rhs.mCursorY),
-  mContents(rhs.mContents) {
-}
 
-void WindowManager::RelocateScreen(int offsetCursorX, int offsetCursorY) {
-    mScreen->mCursorX += offsetCursorX;
-    mScreen->mCursorY += offsetCursorY;
-    mScreen->AdjustCursor();
+
 }
--- src/chapter12/chapter12.h
+++ src/chapter12/chapter12.h
@@ -4,84 +4,35 @@
 #include <string>
 #include <iostream>
 
-class SalesItem12 {
-public:
-	SalesItem12();
-	SalesItem12(const char *isbn, int count, double revenue);
-	SalesItem12(const std::string &isbn, int count, double revenue);
+namespace chapter12 {
 
-	inline double Average() const {
-		if (!mCount) {
-			return 0.0;
-		} else {
-			return mRevenue / mCount;
-		}
+class SalesItem {
+private:
+	std::string mISBN;
+	int 		mCount;
+	double 		mRevenue;
+
+public:
+	SalesItem(const std::string &isbn)
+	: mISBN(isbn),
+	  mCount(0),
+	  mRevenue(0.0) {
 	}
 
-	void Dump(std::ostream &out);
-	void Dump(std::ostream &out) const;
-
-	SalesItem12 &operator=(const SalesItem12 &rhs);
-
-private:
-	std::string		mISBN;
-	int 			mCount;
-	double			mRevenue;
-};
-
-class Person {
-public:
-	Person();
-	Person(const char *name, const char *person);
-
-	const std::string &Name() const {return mName;}
-	const std::string &Address() const {return mAddress;}
-
-private:
-	std::string 	mName;
-	std::string		mAddress;
-};
-
-class Screen {
-public:
-	Screen(int width, int height);
-	Screen(const Screen &rhs);
-
-	char Get() const {
-		return mContents[mCursorY * mWidth + mCursorY];
+	SalesItem(const std::string &isbn, int count, double revenue)
+	: mISBN(isbn),
+	  mCount(count),
+	  mRevenue(revenue) {
 	}
 
-	Screen &Move(int x, int y) {
-		mCursorX = x;
-		mCursorY = y;
-		this->AdjustCursor();
-		return *this;
+	void UpdateCountAndRevenue(int count, double revenue) {
+		mCount = count;
+		mRevenue = revenue;
 	}
 
-private:
-	void AdjustCursor();
-
-	friend class WindowManager;
-private:
-	int 		mWidth;
-	int 		mHeight;
-	int 		mCursorX, mCursorY;
-	std::string mContents;
+	double Average() const;
 };
 
-class WindowManager {
-public:
-	explicit WindowManager(Screen *screen) :
-	  mScreen(screen) {
-	}
-
-private:
-	void RelocateScreen(int offsetCursorX, int offsetCursorY);
-
-private:
-	Screen *mScreen;
-};
-
-void chapter12_1_tutorial();
+}
 
 #endif // MAIN_CHAPTER12_CHAPTER12_H
